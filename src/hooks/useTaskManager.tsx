3a6b9153316f6b889f17a45899339be3d9f04d11
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-03-10 22:57:36 +0300

    refactor: create useTaskManager

---
+++ src/hooks/useTaskManager.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+interface Task {
+ id: string;
+ title: string;
+}
+
+function useTaskManager() {
+ const [tasks, setTasks] = useState<Task[]>([]);
+ const [searchKeyword, setSearchKeyword] = useState<string>("");
+
+ const addTask = (title: string) => {
+    if (title.length < 1) {
+      return;
+    }
+
+    const newTask: Task = {
+      id: nanoid(),
+      title,
+    };
+    setTasks((prev) => [...prev, newTask]);
+ };
+
+ const completeTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+ };
+
+ const updateTask = (id: string, taskUpdate: Task) => {
+    setTasks(tasks.map((task) => (task.id === id ? taskUpdate : task)));
+ };
+
+ const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(searchKeyword.toLowerCase()),
+ );
+
+ return {
+    tasks: filteredTasks,
+    addTask,
+    completeTask,
+    updateTask,
+    setSearchKeyword,
+ };
+}
+
+export default useTaskManager;
